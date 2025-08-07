a9d5ac665696dba25865041f6b16f362185c6a31
Jaypatel123 <jaypatel97043@gmail.com>
2025-08-07 13:12:13 -0400

    adding find_pivot_index and minimux_size leetcode questions

---
+++ Minimum_size_subarray.py
@@ -0,0 +1,31 @@
+# Minimum Size Subarray Sum
+# https://leetcode.com/problems/minimum-size-subarray-sum/?envType=problem-list-v2&envId=prefix-sum
+# Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+# Example 1:
+
+# Input: target = 7, nums = [2,3,1,2,4,3]
+# Output: 2
+# Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+# Example 2:
+
+# Input: target = 4, nums = [1,4,4]
+# Output: 1
+# Example 3:
+
+# Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+# Output: 0
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        array_min =  float('inf')
+        l = 0
+        summ = 0
+        for r in range(len(nums)):
+            summ += nums[r]
+
+            while summ >= target:
+                array_min = min(array_min, r-l+1)
+                summ -= nums[l]
+                l += 1
+        return array_min if array_min != float('inf') else 0
