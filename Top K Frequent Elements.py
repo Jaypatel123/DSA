f8b186e55f110d0509fdf8f23b4cd79561a4624a
Jaypatel123 <jaypatel97043@gmail.com>
2025-08-16 10:28:56 -0400

    Top K frequent element heap and hashmap

---
+++ Top K Frequent Elements.py
@@ -0,0 +1,43 @@
+# 347. Top K Frequent Elements
+
+# Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,1,1,2,2,3], k = 2
+# Output: [1,2]
+# Example 2:
+
+# Input: nums = [1], k = 1
+# Output: [1]
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -104 <= nums[i] <= 104
+# k is in the range [1, the number of unique elements in the array].
+# It is guaranteed that the answer is unique.
+ 
+
+# Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+import heapq
+from collections import Counter 
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hash_map = Counter(nums)
+        heap = []
+        for key, val in hash_map.items():
+            if len(heap) < k:
+                heapq.heappush(heap, (val, key))
+            else:
+                heapq.heappushpop(heap, (val, key))
+        return [h[1] for h in heap] 
+
+
+        
+        
+        
