7d77e463189283062e201196417fe75815e01237
Jaypatel123 <jaypatel97043@gmail.com>
2025-09-08 22:57:07 -0400

    adding amazon problem

---
+++ distributing coupons amazon.py
@@ -0,0 +1,14 @@
+Amazon.com is distributing coupons in the form of a lottery system for loyal customers. The coupons are called "lucky numbers" and the customer with the largest lucky number gets the best discount. Devise a method to determine the maximum possible lucky number. A positive integer is a lucky number if its decimal representation contains onlydigits x and y. For example, if x = 2 and y = 5, then 2,552, and 5225 are lucky numbers, and 3, 24, 57 and 389 are not.
+
+Given two different digits x and y and a positive integer n, determine the maximum possible lucky number, the sum of whose digits is n. It is guaranteed that at least one lucky number exists for the given x, y, and n.
+
+Example:
+
+If the two digits that make up the number are x = 3 and y = 4, and the sum of the digits must be n = 13, then the lucky numbers are:
+    
+    • 3334
+    • 3343
+    • 3433
+    • 4333
+    
+    The maximum lucky number among these is 4333.
