a3fe41d58a4bb488b69d71226be1aab6ca73a0ee
Jaypatel123 <jaypatel97043@gmail.com>
2025-09-29 23:25:08 -0400

    added number of islands

---
+++ Number of Islands.py
@@ -0,0 +1,56 @@
+# 200. Number of Islands
+
+# Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+# An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+ 
+
+# Example 1:
+
+# Input: grid = [
+#   ["1","1","1","1","0"],
+#   ["1","1","0","1","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","0","0","0"]
+# ]
+# Output: 1
+# Example 2:
+
+# Input: grid = [
+#   ["1","1","0","0","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","1","0","0"],
+#   ["0","0","0","1","1"]
+# ]
+# Output: 3
+ 
+
+# Constraints:
+
+# m == grid.length
+# n == grid[i].length
+# 1 <= m, n <= 300
+# grid[i][j] is '0' or '1'.
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        m = len(grid) # row
+        n = len(grid[0]) # col
+        def island(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] != '1':
+                return
+            else:
+                grid[i][j] = '0'
+                island(i, j+1) # Right
+                island(i+1, j) # Down
+                island(i, j-1) # left
+                island(i-1, j) # up
+
+        for row in range(m):
+            for col in range(n):
+                if grid[row][col] == '1':
+                    count += 1
+                    island(row, col)
+        return count # Time: O(m * n)
